3ea71357c78222f42e8d32ae4ae7d750a3c3c9b6
INFINITE <ceoinfiniteunion@gmail.com>
2026-07-20 20:03:45 +0300

    feat: Railway deployment config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
+  output: 'standalone',
+}
 
 module.exports = nextConfig
