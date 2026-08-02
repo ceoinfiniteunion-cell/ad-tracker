2a1ab618454d251be3c7cd2e91b0d20a44bcfb74
INFINITE <ceoinfiniteunion@gmail.com>
2026-08-02 14:19:20 +0300

    feat: js ripple effect on click

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { RippleEffect } from '@/components/layout/RippleEffect'
 import { BgImage } from '@/components/layout/BgImage'
 import { Providers } from '@/components/layout/Providers'
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="uk">
       <body className={inter.className}>
-        <Providers><BgImage />{children}</Providers>
+        <Providers><BgImage /><RippleEffect />{children}</Providers>
       </body>
     </html>
   )
